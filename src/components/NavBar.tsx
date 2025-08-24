a2a0f14cedf4f68e4ebdae5e17a3e750274f35c9
Gustavo A. Verneck <gustavoverneck@gmail.com>
2025-08-24 00:08:10 -0300

    feat: enhance NavBar with dropdown menu and add fade-in animation to HomePage

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,37 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 export default function NavBar () {
+    const [open, setOpen] = useState(false);
+
     return (
-        <nav className="flex gap-4 p-4 bg-black text-white px-10">
-            <Link to="/">Home</Link>
-            <Link to="/counter">Counter</Link>
+        <nav className="flex gap-4 p-4 bg-black text-white px-10 items-center">
+            <Link 
+                to="/"
+                className="py-2 px-4 hover:bg-white hover:text-black rounded"
+            >
+                Home
+            </Link>
+            <div className="relative">
+                <button
+                    className="bg-black rounded px-4 py-2 hover:bg-white hover:text-black"
+                    onClick={() => setOpen((v) => !v)}
+                >
+                    Examples ▼
+                </button>
+                {open && (
+                    <div className="absolute left-0 mt-2 bg-black shadow-lg flex flex-col min-w-[120px] z-10 border-l-2 rounded border-white">
+                        <Link 
+                            to="/counter"
+                            className="px-4 py-2 hover:bg-white hover:text-black rounded"
+                            onClick={() => setOpen(false)}
+                        >
+                            Counter
+                        </Link>
+                        {/* {outros} */}
+                    </div>
+                )}
+            </div>
         </nav>
     )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 export default function HomePage () {
     return (
-        <div className="flex flex-col items-center justify-center">
-            <h1 className="text-4xl">
-                React Playground
-            </h1>
-            <p className="pt-10 text-1xl">
-                Gustavo A. Verneck
-            </p>
+        <div className="flex items-center justify-center min-h-screen bg-black">
+            <div className="bg-transparent bg-opacity-90 rounded-xl shadow-lg p-10 animate-fadeIn flex flex-col items-center">
+                <h1 className="text-4xl hover:scale-110 transition-transform duration-300">
+                    React Playground
+                </h1>
+                <p className="pt-10 text-xl">
+                    Gustavo A. Verneck
+                </p>
+            </div>
         </div>
     )
 }
